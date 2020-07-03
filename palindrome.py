0f4340c0bd99eba8d914d4f19d99bd80fba39124
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-07-02 21:34:52 -0500

    Checks a word or phrase

---
+++ palindrome.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+def is_palindrome(word):
+    """
+    checks if a word or phrase is palindrome
+    """
+    word = word.replace(" ", "")
+    new_word = word.lower()
+
+    return new_word == new_word[::-1]
+
+
+def run():
+    """ Run the code """
+    word = str(input("Write a word or phrase: "))
+    result = is_palindrome(word)
+    if result is True:
+        print(f"{word} is palindrome")
+    else:
+        print(f"{word} no is palindrome")
+
+
+if __name__ == "__main__":
+    run()
